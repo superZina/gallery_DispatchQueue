fb471c29f7b208a39a09bdd76c1803deebe9b1b9
이진하 <dkwlsfk2323@gmail.com>
2022-04-15 00:17:28 +0900

    getPhotoList 함수 구현 및 디코딩 함수 구현

--- Gallery_DispatchQueue/Gallery_DispatchQueue/APIManager.swift
+++ Gallery_DispatchQueue/Gallery_DispatchQueue/APIManager.swift
@@ -8,16 +8,63 @@
 import Foundation
 
 class APIManager {
+    // MARK: - Errors
+    enum ServerError: Error {
+        case unknown
+    }
+
+    enum NetworkError: Error {
+        case disconnected
+    }
+}
+
+extension APIManager {
     
     // 최초에 사진 리스트를 받아오는 함수.
     // 비동기로 작동해야하고
     // 받아온 이후에 컴플리션 핸들러로 사진 배열을 넘겨줘야 한다.
-    func getPhotoList(completionHandler: @escaping (Error, [Photo]) -> ()) {
-        let url = URL(string: "\(Config.BaseURL)")
+    func getPhotoList(completionHandler: @escaping (Error?, [Photo]?) -> ()) {
+        
+        guard let url = URL(string: "\(Config.BaseURL)") else {
+            completionHandler(NetworkError.disconnected, nil)
+            return
+        }
+        // Authorization Header
         let urlSessionConfig = URLSessionConfiguration.default
-        urlSessionConfig.httpAdditionalHeaders = 
+        urlSessionConfig.httpAdditionalHeaders = Config.AuthorizationHeader
         
+        let session = URLSession(configuration: urlSessionConfig)
         
-        URLSession.shared.dataTask(with: <#T##URL#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+        // request and response
+        session.dataTask(with: url) { data, response, error in
+            
+            switch (response as? HTTPURLResponse)?.statusCode ?? 500 {
+            case 200:
+                let photoList = self.decoded(data: data)
+                
+                if photoList != nil {
+                    completionHandler(nil, photoList!)
+                }else{
+                    completionHandler(ServerError.unknown, nil)
+                }
+            default:
+                completionHandler(ServerError.unknown, nil)
+            }
+        }
+    }
+    
+    // Data를 디코딩하여 Photo 배열로 리턴하는 디코딩 함수.
+    func decoded(data: Data?) -> [Photo]? {
+        let decoder = JSONDecoder()
+        do {
+            guard let photoData = data else { return nil }
+            let photoList = try decoder.decode([Photo].self, from: photoData)
+            return photoList
+        } catch {
+            return nil
+        }
     }
 }
+
+
+
