c1e66f3abc2f32f393148ba1750a188ae82dad10
이진하 <dkwlsfk2323@gmail.com>
2022-04-19 00:26:00 +0900

    각 CollectionViewCell에 ActivityIndicator 추가

--- Gallery_DispatchQueue/Gallery_DispatchQueue/View/CollectionViewCell.swift
+++ Gallery_DispatchQueue/Gallery_DispatchQueue/View/CollectionViewCell.swift
@@ -10,17 +10,28 @@ import UIKit
 class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     let photoLoader: PhotoLoader = PhotoLoader()
     
     func config(url: String?, viewModel: MainViewModel) {
         let urlString = url ?? ""
+        
+        DispatchQueue.main.async {
+            self.activityIndicator.isHidden = false
+            self.activityIndicator.startAnimating()
+        }
+        
         viewModel.load(from: urlString, photoLoder: self.photoLoader) { data in
             if let imageData = data {
                 self.photoView.image = UIImage(data: imageData)
             }else {
                 self.photoView.image = UIImage(named: Config.defaultImageName)!
             }
+            self.activityIndicator.startAnimating()
+            self.activityIndicator.isHidden = true
         }
+        
     }
     
     override func prepareForReuse() {
