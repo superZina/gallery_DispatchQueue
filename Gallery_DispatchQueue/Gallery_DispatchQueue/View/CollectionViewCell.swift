d9df42067eab6c736d251d6a9b875fddde0f746c
이진하 <dkwlsfk2323@gmail.com>
2022-04-22 00:01:49 +0900

    estimated size none변경

--- Gallery_DispatchQueue/Gallery_DispatchQueue/View/CollectionViewCell.swift
+++ Gallery_DispatchQueue/Gallery_DispatchQueue/View/CollectionViewCell.swift
@@ -12,23 +12,23 @@ var imageCache = [String: UIImage]()
 
 class CollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var photoView: CustomImageView!
     private let defaultImage = UIImage(named: Config.defaultImageName)!
     
     func config(indexPath: IndexPath, viewModel: MainViewModel) {
         photoView.image = nil
-//        photoView.isLoading = true
+        photoView.isLoading = true
         
         viewModel.load(indexPath: indexPath) { data in
-//            self.photoView.setImage(from: image)
-//            self.photoView.isLoading = false
-            DispatchQueue.main.async {
-                if let imageData = data {
-                    self.photoView.image = UIImage(data: imageData)
-                }else {
-                    self.photoView.image = self.defaultImage
-                }
-            }
+            self.photoView.setImage(from: data)
+            self.photoView.isLoading = false
+//            DispatchQueue.main.async {
+//                if let imageData = data {
+//                    self.photoView.image = UIImage(data: imageData)
+//                }else {
+//                    self.photoView.image = self.defaultImage
+//                }
+//            }
         }
     }
 }
